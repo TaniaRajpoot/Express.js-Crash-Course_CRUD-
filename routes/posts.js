e224558c6442428fb4ce92a53defdb0fd5183d95
TaniaRajpoot <rajpoottania299@gmail.com>
2025-06-04 11:07:35 +0500

    Restored stashed changes and completed feature

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { createPost, deletePost, getPost, getPosts, updatePost } from '../controller/postController.js'
 const router = express.Router();
 
 //setup staric folder
@@ -16,66 +17,23 @@ const router = express.Router();
 
 // });
 
-let posts = [
-   {id: 1, title: 'Post One'},
-   {id: 2, title: 'Post Two'},
-   {id: 3, title: 'Post Three'}
-];
+
+
+
 
 //get all post 
-router.get('/', (req,res) =>{
-    const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  } else {
-    res.status(200).json(posts);
-  }
-});
+router.get('/', getPosts);
 
 //get single post 
-router.get('/:id', (req,res) =>{
-    const id = parseInt(req.params.id);
-    // res.status(200).json(posts.filter((post) => post.id === id));
-
-  const post = posts.find((post) => post.id === id);
-
-    if(!post){
-        return res
-        .status(404)
-        .json({msg:`A post with the id of ${id} was not found`});
-    }
-        res.status(200).json(post);
-
-});
+router.get('/:id',getPost );
 
 //create new post 
-router.post('/',(req, res) => {
-//    console.log(req.body);
- const newPost = {
-  id: posts.length +1,
-  title: req.body.title
- };
-  if(!newPost.title){
-   return res.status(404).json({msg:`please include a title`});
-    }
+router.post('/',createPost); 
 
-  posts.push(newPost);
-  res.status(201).json(posts);
-}); 
+//update post
+router.put('/:id',updatePost );
 
-// //update post
-// router.put('/:id', (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const post = posts.find((post) => post.id === id );
-
-//   if(!post){
-//     return res
-//     .status(404)
-//     .json({msg:`A post with the id of ${id} was not found `});
-//   }
-//   post.title = req.body.title;
-//   res.status(200).json(posts);
-// });
+// delete post
+router.delete('/:id', deletePost );
 
 export default router;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import express from "express";
 import path from "path";
 import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
+import { fileURLToPath } from "url";
 const port = process.env.port || 8000;
+
+//Get the dierctory name 
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -9,9 +18,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//logger middleware 
+app.use(logger);
+//setup static folder
+app.use(express.static(path.join(__dirname,'public')));
+
 // Routes
 app.use('/api/posts', posts);
 
+
+
+//error handler
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => console.log(`server is runing on port ${port}`));
